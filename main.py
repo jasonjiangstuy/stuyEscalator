274a4ad0d698d3c5af2363f58d1c75f52b7ca8c6
jasonjiangstuy <jasonjiangny@gmail.com>
2020-04-04 20:37:15 -0400

    js bugging

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ app = Flask(__name__, static_folder='./static', template_folder='./templates')
 app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
 from google.cloud import datastore
-
+import datetime
 def resetMaindata():
-    mainData = datastore.Entity(
-      exclude_from_indexes=['created', 'purpose'])
-    default = (False, False)
+    key = client.key('Escalators', 1234)
+    mainData = datastore.Entity(key, exclude_from_indexes=['created', 'purpose'])
+    default = [False, False]
     mainData.update({
-        'created': datetime.datetime.utcnow(),
-        'purpose': 'status',
+        # 'created': datetime.datetime.utcnow(),
+        # 'purpose': 'status',
         # up down
         '23': default,
-        '24': ('N/A'),
+        '24': ['N/A', 'N/A'],
         '35': default,
         '46': default,
         '57': default,
@@ -25,6 +25,7 @@ def resetMaindata():
         # ,'done': False
     })
     client.put(mainData)
+    return key
 
 #on startup 
 def create_client(project_id):
@@ -34,36 +35,58 @@ client = create_client('jasontestingke')
 mainkey = resetMaindata()
 
 def changeStatus(key, value):
-  query = client.query()
-  result = query.add_filter('purpose', '=', 'status').fetch()
-  if not data:
+  query = client.get(mainkey)
+#   result = query.add_filter('purpose', '=', 'status').fetch()
+  if not query:
       raise ValueError(
           'Database does not exist.')
 
-  result[key] = value
+  query[key] = value
 
-  client.put(result) 
+  client.put(query) 
 
 def listWorking(): # get the set of working a not working escalators
-  query = client.query()
-  result = query.add_filter('purpose', '=', 'status').fetch()
+  query = client.get(mainkey)
+  result = query
+#   result = query.add_filter('purpose', '=', 'status').fetch()
   if not result:
       raise ValueError(
           'Database does not exist.')
-  return sorted(result.items())
-    
 
-  
-
-
+  result = sorted(result.items())
+#   print(result)
+  index = 0
+  final = []
+  for i in result: 
+    temp = []
+    key = i[0]
+    try:
+        key = key[0] + ' to ' + key[1] # make a more readable table
+    except:
+        ValueError('Key for database messed up---', key)
+    # result[index][0] = 
+    temp.append(key)
+    value = i[1]
+    for i in value:
+        if i == 'N/A':
+            temp.append(i)
+        elif i: # if not False
+            temp.append('Working')
+        else:
+            temp.append('Not Working')
+    index += 1
+    final.append(temp)
+      
+  return final
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-  
+  final = []
   rulebook = listWorking()
+  print(rulebook)
   return render_template(
-    # 'index.html', rulebook= 
+     'index.html', rulebook=rulebook
   )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='3000')
+    app.run(host='0.0.0.0', port='3030')
---
+++ static/js/index.js
@@ -0,0 +1,97 @@
+$('#submit').click(function (){
+	var sendRequest = true;
+    const target = $('#escalator').val();
+    console.log(target);
+
+    // checkboxes
+    var radioButton = document.getElementsByName('status');
+
+    const status = (function(){
+      var temp = []
+      for(i = 0; i < radioButton.length; i++) {
+        if (radioButton[i].checked){
+          temp.push(radioButton[i].value);
+        }
+      };
+      return temp
+    });
+    // ();
+    
+    // const curFirst = $('#cfirstname').val();
+
+    // const newFirst = $('#firstname').val();
+    // console.log(newFirst);
+    // const curLast = $('#clastname').val()
+    // const newLast = $('#lastname').val();
+    // console.log(newLast);
+    // const newprofilepic = $('#profilepic').prop('files')[0];
+    // console.log(newprofilepic);
+
+    var form = $('.accountForm')[0];
+    var fd = new FormData(form);
+
+    if (target != "" && target != undefined){
+        console.log('form includes target');
+        fd.append('newUsername', "true");
+    }else {
+        if (sendRequest){
+            sendRequest = false;
+        }
+        fd.append('newUsername', "false");
+    }
+
+    if (newFirst != curFirst && newFirst != "" && newFirst != undefined){
+        console.log('form includes first');
+        sendRequest = true;
+        fd.append('newFirst', "true");
+    }else {
+        fd.append('newFirst', "false");
+    }
+    if (newLast != curLast && newLast != "" && newLast != undefined){
+        console.log('form includes last');
+        sendRequest = true;
+        fd.append('newLast', "true");
+    }else {
+        fd.append('newLast', "false");
+    }
+    if (newprofilepic != undefined){
+        console.log('form includes pic 2,0');
+        sendRequest = true;
+        fd.append('newPFP', "true");
+    }else {
+        fd.append('newPFP', "false");
+    }
+
+
+    if (sendRequest){
+        $.ajax({
+    type : 'POST',
+    url : '/myAccount',
+    data: fd,
+    processData: false,  // tell jQuery not to process the data
+    contentType: false,   // tell jQuery not to set contentType
+    success: function(data) {
+        if (data == "success"){
+        alert("edits successful");
+                    window.location.reload(true);
+        }else {
+        alert(data);
+                    window.location.reload(true);
+        }
+    },
+    error: function(e) {
+        console.log(e);
+    }
+    });
+    }
+    else {
+        alert("Not all forms are filled canceling request");
+    }
+
+				//
+				//
+				// var form = $('#toGCS')[0]
+				// var GCS = new FormData(form)
+
+
+  });
